5acd97311cbbac33a37092d9feb61ab346481840
Pawel Jaskula <pj.jaskula@gmail.com>
2017-05-13 14:02:04 +0200

    WindowsFormApplication
    Add WindowsFormApplication with design and code

---
+++ Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace JezioraSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chb_strefaciszy = new System.Windows.Forms.CheckBox();
+            this.chb_kapielisko = new System.Windows.Forms.CheckBox();
+            this.txt_ident = new System.Windows.Forms.TextBox();
+            this.txt_nazwa = new System.Windows.Forms.TextBox();
+            this.txt_powierzchnia = new System.Windows.Forms.TextBox();
+            this.txt_glebokosc = new System.Windows.Forms.TextBox();
+            this.txt_miejscowosc = new System.Windows.Forms.TextBox();
+            this.btn_first = new System.Windows.Forms.Button();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_last = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_new = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.Msg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Indentyfikator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazwa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Powierzchnia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Głębokość";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Miejscowość";
+            // 
+            // chb_strefaciszy
+            // 
+            this.chb_strefaciszy.AutoSize = true;
+            this.chb_strefaciszy.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.chb_strefaciszy.Location = new System.Drawing.Point(16, 217);
+            this.chb_strefaciszy.Name = "chb_strefaciszy";
+            this.chb_strefaciszy.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chb_strefaciszy.Size = new System.Drawing.Size(121, 26);
+            this.chb_strefaciszy.TabIndex = 5;
+            this.chb_strefaciszy.Text = "Strefa ciszy";
+            this.chb_strefaciszy.UseVisualStyleBackColor = true;
+            // 
+            // chb_kapielisko
+            // 
+            this.chb_kapielisko.AutoSize = true;
+            this.chb_kapielisko.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.chb_kapielisko.Location = new System.Drawing.Point(16, 249);
+            this.chb_kapielisko.Name = "chb_kapielisko";
+            this.chb_kapielisko.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chb_kapielisko.Size = new System.Drawing.Size(111, 26);
+            this.chb_kapielisko.TabIndex = 6;
+            this.chb_kapielisko.Text = "Kąpielisko";
+            this.chb_kapielisko.UseVisualStyleBackColor = true;
+            // 
+            // txt_ident
+            // 
+            this.txt_ident.Location = new System.Drawing.Point(151, 28);
+            this.txt_ident.Name = "txt_ident";
+            this.txt_ident.Size = new System.Drawing.Size(100, 20);
+            this.txt_ident.TabIndex = 7;
+            // 
+            // txt_nazwa
+            // 
+            this.txt_nazwa.Location = new System.Drawing.Point(151, 65);
+            this.txt_nazwa.Name = "txt_nazwa";
+            this.txt_nazwa.Size = new System.Drawing.Size(100, 20);
+            this.txt_nazwa.TabIndex = 8;
+            // 
+            // txt_powierzchnia
+            // 
+            this.txt_powierzchnia.Location = new System.Drawing.Point(151, 104);
+            this.txt_powierzchnia.Name = "txt_powierzchnia";
+            this.txt_powierzchnia.Size = new System.Drawing.Size(100, 20);
+            this.txt_powierzchnia.TabIndex = 9;
+            // 
+            // txt_glebokosc
+            // 
+            this.txt_glebokosc.Location = new System.Drawing.Point(151, 147);
+            this.txt_glebokosc.Name = "txt_glebokosc";
+            this.txt_glebokosc.Size = new System.Drawing.Size(100, 20);
+            this.txt_glebokosc.TabIndex = 10;
+            // 
+            // txt_miejscowosc
+            // 
+            this.txt_miejscowosc.Location = new System.Drawing.Point(151, 185);
+            this.txt_miejscowosc.Name = "txt_miejscowosc";
+            this.txt_miejscowosc.Size = new System.Drawing.Size(100, 20);
+            this.txt_miejscowosc.TabIndex = 11;
+            // 
+            // btn_first
+            // 
+            this.btn_first.AutoSize = true;
+            this.btn_first.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_first.Location = new System.Drawing.Point(282, 37);
+            this.btn_first.Name = "btn_first";
+            this.btn_first.Size = new System.Drawing.Size(91, 32);
+            this.btn_first.TabIndex = 12;
+            this.btn_first.Text = "Pierwszy";
+            this.btn_first.UseVisualStyleBackColor = true;
+            this.btn_first.Click += new System.EventHandler(this.btn_first_Click);
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.AutoSize = true;
+            this.btn_prev.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_prev.Location = new System.Drawing.Point(379, 37);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(100, 32);
+            this.btn_prev.TabIndex = 13;
+            this.btn_prev.Text = "Poprzedni";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.AutoSize = true;
+            this.btn_next.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_next.Location = new System.Drawing.Point(485, 37);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(96, 32);
+            this.btn_next.TabIndex = 14;
+            this.btn_next.Text = "Następny";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_last
+            // 
+            this.btn_last.AutoSize = true;
+            this.btn_last.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_last.Location = new System.Drawing.Point(590, 37);
+            this.btn_last.Name = "btn_last";
+            this.btn_last.Size = new System.Drawing.Size(91, 32);
+            this.btn_last.TabIndex = 15;
+            this.btn_last.Text = "Ostatni";
+            this.btn_last.UseVisualStyleBackColor = true;
+            this.btn_last.Click += new System.EventHandler(this.btn_last_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.AutoSize = true;
+            this.btn_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_update.Location = new System.Drawing.Point(282, 92);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(96, 32);
+            this.btn_update.TabIndex = 16;
+            this.btn_update.Text = "Aktualizuj";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.AutoSize = true;
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_delete.Location = new System.Drawing.Point(388, 92);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(91, 32);
+            this.btn_delete.TabIndex = 17;
+            this.btn_delete.Text = "Usuń";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_new
+            // 
+            this.btn_new.AutoSize = true;
+            this.btn_new.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_new.Location = new System.Drawing.Point(340, 142);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(91, 32);
+            this.btn_new.TabIndex = 18;
+            this.btn_new.Text = "Nowy";
+            this.btn_new.UseVisualStyleBackColor = true;
+            this.btn_new.Click += new System.EventHandler(this.btn_new_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.AutoSize = true;
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_add.Location = new System.Drawing.Point(282, 186);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(91, 32);
+            this.btn_add.TabIndex = 19;
+            this.btn_add.Text = "Dodaj";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.AutoSize = true;
+            this.btn_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_cancel.Location = new System.Drawing.Point(388, 186);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(91, 32);
+            this.btn_cancel.TabIndex = 19;
+            this.btn_cancel.Text = "Powrót";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // Msg
+            // 
+            this.Msg.AutoSize = true;
+            this.Msg.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Msg.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Msg.Location = new System.Drawing.Point(317, 268);
+            this.Msg.Name = "Msg";
+            this.Msg.Size = new System.Drawing.Size(0, 22);
+            this.Msg.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 434);
+            this.Controls.Add(this.Msg);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_new);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_last);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.btn_first);
+            this.Controls.Add(this.txt_miejscowosc);
+            this.Controls.Add(this.txt_glebokosc);
+            this.Controls.Add(this.txt_powierzchnia);
+            this.Controls.Add(this.txt_nazwa);
+            this.Controls.Add(this.txt_ident);
+            this.Controls.Add(this.chb_kapielisko);
+            this.Controls.Add(this.chb_strefaciszy);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chb_strefaciszy;
+        private System.Windows.Forms.CheckBox chb_kapielisko;
+        private System.Windows.Forms.TextBox txt_ident;
+        private System.Windows.Forms.TextBox txt_nazwa;
+        private System.Windows.Forms.TextBox txt_powierzchnia;
+        private System.Windows.Forms.TextBox txt_glebokosc;
+        private System.Windows.Forms.TextBox txt_miejscowosc;
+        private System.Windows.Forms.Button btn_first;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_last;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Label Msg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JezioraSQL
+{
+    public partial class Form1 : Form
+    {
+        DataTable Tabela; SqlCommand Sta;
+        int Pozycja;
+        void Wstaw_Dane()
+        {
+            txt_ident.Text = Tabela.Rows[Pozycja]["ident"].ToString();
+            txt_nazwa.Text = Tabela.Rows[Pozycja]["nazwa_jeziora"].ToString();
+            txt_powierzchnia.Text = Tabela.Rows[Pozycja]["powierzchnia_km"].ToString();
+            txt_glebokosc.Text = Tabela.Rows[Pozycja]["glebokosc_m"].ToString();
+            txt_miejscowosc.Text = Tabela.Rows[Pozycja]["miejscowosc"].ToString();
+            if (int.Parse(Tabela.Rows[Pozycja]["strefa_ciszy"].ToString()) == 1)
+                chb_strefaciszy.Checked = true;
+            else
+                chb_strefaciszy.Checked = false;
+            if (int.Parse(Tabela.Rows[Pozycja]["kapielisko"].ToString()) == 1)
+                chb_kapielisko.Checked = true;
+            else
+                chb_kapielisko.Checked = false;
+
+            Msg.Text = "OK";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            txt_ident.Enabled = false;
+            btn_add.Visible = false;
+            btn_cancel.Visible = false;
+            SqlConnection MyConn = new SqlConnection(global::JezioraSQL.Properties.Settings.Default.JezioraConnectionString);
+            try
+            {
+                MyConn.Open();
+            }
+            catch (Exception ex) { Msg.Text = "Brak połączenia z Bazą danych"; return; }
+            Sta = MyConn.CreateCommand();
+            Sta.CommandText = "select * from Jeziora";
+            SqlDataReader Reader = Sta.ExecuteReader();
+            Tabela = new DataTable(); Tabela.Load(Reader);
+            Pozycja = 0;
+            if (Tabela.Rows.Count > 0) Wstaw_Dane();
+            else Msg.Text = "brak wierszy w tabeli";
+        }
+
+        private void btn_first_Click(object sender, EventArgs e)
+        {
+            if (Tabela.Rows.Count > 0)
+            {
+                Pozycja = 0; Wstaw_Dane();
+            }
+            else Msg.Text = "brak wierszy w tabeli";
+        }
+
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            if (Pozycja > 0)
+            {
+                Pozycja--; Wstaw_Dane();
+            }
+            else Msg.Text = "początek tabeli";
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            if (Pozycja < Tabela.Rows.Count - 1)
+            {
+                Pozycja++; Wstaw_Dane();
+            }
+            else Msg.Text = "koniec tabeli";
+        }
+
+        private void btn_last_Click(object sender, EventArgs e)
+        {
+            if (Tabela.Rows.Count > 0)
+            {
+                Pozycja = Tabela.Rows.Count - 1; Wstaw_Dane();
+            }
+            else Msg.Text = "brak wierszy w tabeli";
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            
+            Sta.CommandText = "update Jeziora set nazwa_jeziora='" + txt_nazwa.Text + "' where ident=" + txt_ident.Text;
+            Sta.CommandText = "update Jeziora set powierzchnia_km=" + txt_powierzchnia.Text + " where ident=" + txt_ident.Text;
+            Sta.CommandText = "update Jeziora set glebokosc_m=" + txt_glebokosc.Text + " where ident=" + txt_ident.Text;
+            Sta.CommandText = "update Jeziora set miejscowosc='" + txt_miejscowosc.Text + "' where ident=" + txt_ident.Text;
+            if (chb_strefaciszy.Checked == true)
+                Sta.CommandText = "update Jeziora set strefa_ciszy=" + 1 + " where ident=" + txt_ident.Text;
+            else
+                Sta.CommandText = "update Jeziora set strefa_ciszy=" + 0 + " where ident=" + txt_ident.Text;
+            if (chb_kapielisko.Checked == true)
+                Sta.CommandText = "update Jeziora set kapielisko=" + 1 + " where ident=" + txt_ident.Text;
+            else
+                Sta.CommandText = "update Jeziora set kapielisko=" + 0 + " where ident=" + txt_ident.Text;
+
+            Tabela.Rows[Pozycja]["nazwa_jeziora"] = txt_nazwa.Text;
+            Tabela.Rows[Pozycja]["powierzchnia_km"] = txt_powierzchnia.Text;
+            Tabela.Rows[Pozycja]["glebokosc_m"] = txt_glebokosc.Text;
+            Tabela.Rows[Pozycja]["miejscowosc"] = txt_miejscowosc.Text;
+            if (chb_strefaciszy.Checked == true)
+            {
+                chb_strefaciszy.Checked = true;
+                Tabela.Rows[Pozycja]["strefa_ciszy"] = 1;
+            }
+
+            else
+            {
+                chb_strefaciszy.Checked = false;
+                Tabela.Rows[Pozycja]["strefa_ciszy"] = 0;
+            }
+
+            if (chb_kapielisko.Checked == true)
+            {
+                chb_kapielisko.Checked = true;
+                Tabela.Rows[Pozycja]["kapielisko"] = 1;
+            }
+
+            else
+            {
+                chb_kapielisko.Checked = false;
+                Tabela.Rows[Pozycja]["kapielisko"] = 0;
+            }
+
+
+            Msg.Text = "Updated";
+
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            Sta.CommandText = "delete from Jeziora where ident=" + txt_ident.Text;
+            Sta.ExecuteNonQuery();
+            Tabela.Rows.RemoveAt(Pozycja);
+            if (Pozycja == Tabela.Rows.Count)
+            {
+                Pozycja--;
+            }
+            Wstaw_Dane();
+            Msg.Text = "Deleted";
+        }
+
+        private void btn_new_Click(object sender, EventArgs e)
+        {
+            btn_add.Visible = true;
+            btn_cancel.Visible = true;
+            btn_delete.Visible = false;
+            btn_last.Visible = false;
+            btn_new.Visible = false;
+            btn_next.Visible = false;
+            btn_first.Visible = false;
+            btn_prev.Visible = false;
+            btn_update.Visible = false;
+            txt_ident.Enabled = true;
+            txt_ident.Text = txt_nazwa.Text = txt_powierzchnia.Text = txt_glebokosc.Text = txt_miejscowosc.Text = "";
+            chb_strefaciszy.Checked = false;
+            chb_kapielisko.Checked = false;
+            Msg.Text = "Wprowadź dane i kliknij Dodaj";
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            btn_add.Visible = false;
+            btn_cancel.Visible = false;
+            btn_delete.Visible = true;
+            btn_last.Visible = true;
+            btn_new.Visible = true;
+            btn_next.Visible = true;
+            btn_first.Visible = true;
+            btn_prev.Visible = true;
+            btn_update.Visible = true;
+            txt_ident.Enabled = false;
+            Sta.CommandText = "insert into Jeziora(ident,nazwa_jeziora,powierzchnia_km,glebokosc_m,miejscowosc,strefa_ciszy,kapielisko) values(@ident,@nazwa_jeziora,@powierzchnia_km,@glebokosc_m,@miejscowosc,@strefa_ciszy,@kapielisko)";
+            Sta.Parameters.AddWithValue("@ident", int.Parse(txt_ident.Text));
+            Sta.Parameters.AddWithValue("@nazwa_jeziora", txt_nazwa.Text);
+            Sta.Parameters.AddWithValue("@powierzchnia_km", int.Parse(txt_powierzchnia.Text));
+            Sta.Parameters.AddWithValue("@glebokosc_m", int.Parse(txt_glebokosc.Text));
+            Sta.Parameters.AddWithValue("@miejscowosc", txt_miejscowosc.Text);
+            if (chb_strefaciszy.Checked == true)
+            {
+                Sta.Parameters.AddWithValue("@strefa_ciszy", 1);
+            }
+            else
+                Sta.Parameters.AddWithValue("@strefa_ciszy", 0);
+            if (chb_kapielisko.Checked == true)
+            {
+                Sta.Parameters.AddWithValue("@kapielisko", 1);
+            }
+            else
+                Sta.Parameters.AddWithValue("@kapielisko", 0);
+            
+            Sta.ExecuteNonQuery();
+            DataRow Wiersz = Tabela.NewRow(); // krwadratowe nawiasy to index property 
+            Wiersz[0] = txt_ident.Text; Wiersz[1] = txt_nazwa.Text; Wiersz[2] = txt_powierzchnia.Text; Wiersz[3] = txt_glebokosc.Text;
+            Wiersz[4] = txt_miejscowosc.Text;
+            if (chb_strefaciszy.Checked == true)
+            {
+                Wiersz[5] = 1;
+            }
+            else
+                Wiersz[5] = 0;
+            if (chb_kapielisko.Checked == true)
+            {
+                Wiersz[6] = 1;
+            }
+            else
+                Wiersz[6] = 0;
+            Tabela.Rows.Add(Wiersz);
+            Pozycja = Tabela.Rows.Count - 1;
+            Msg.Text = "Added";
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+                
+        }
+    }
+}
